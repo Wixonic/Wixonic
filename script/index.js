2e2991fb98f0a5d992908dc30a4cfc91c9a90411
Wixonic <wixonic@icloud.com>
2021-08-27 13:19:08 +0200

    Update my work

--- script/index.js
+++ script/index.js
@@ -1,3 +1,29 @@
+const sortByDate = (object) =>
+{
+	let dateArray = [];
+
+	for (let x in object)
+	{
+		const item = new Date(object[x].date).getTime() + "#" + x;
+
+		if (dateArray.length > 0)
+		{
+			dateArray.push(item);
+		} else {
+			dateArray = [item];
+		}
+	}
+
+	dateArray.sort();
+
+	for (let x in dateArray)
+	{
+		dateArray[x] = object[dateArray[x].split("#")[1]];
+	}
+
+	return dateArray;
+};
+
 onload = () =>
 {
 	GET("/codes")
@@ -8,6 +34,9 @@ onload = () =>
 			try
 			{
 				doc.class("codes-container")[0].innerHTML = "";
+
+				codes = sortByDate(codes);
+				codes.reverse();
 
 				for (let x in codes)
 				{
@@ -23,4 +52,31 @@ onload = () =>
 			doc.class("codes-container")[0].innerHTML = "<b>Not found</b>";
 		}
 	}).catch(() => doc.class("codes-container")[0].innerHTML = "<b>Not found</b>");
+
+	GET("/releases")
+	.then((releases) =>
+	{
+		if (releases)
+		{
+			try
+			{
+				doc.class("releases-container")[0].innerHTML = "";
+
+				releases = sortByDate(releases);
+				releases.reverse();
+
+				for (let x in releases)
+				{
+					const release = releases[x];
+
+					doc.class("releases-container")[0].innerHTML += `<a href="${release.link}" target="_blank" class="release"><div class="title">${release.package} - V. ${release.version}</div><div class="summary">${release.summary[pageLang]}</div><div class="language ${release.language || "null"}">${release.language || "?"}</div></a>`;
+				}
+			} catch(e) {
+				console.log(e);
+				doc.class("releases-container")[0].innerHTML = "<b>An error occured</b>";
+			}
+		} else {
+			doc.class("releases-container")[0].innerHTML = "<b>Not found</b>";
+		}
+	}).catch(() => doc.class("releases-container")[0].innerHTML = "<b>Not found</b>");
 };
